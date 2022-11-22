a49d237d148b56f74f305c632dfe1ce3df73d5a6
aarishlakhani <alakha33@uwo.ca>
2022-11-22 04:20:07 -0500

    nodemon

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.get("/", function (req, res) {
 });
 
 app.get("/api/courses", (req, res) => {
-  res.send([1, 2, 3]);
+  res.send([1, 5, 5]);
 });
 app.listen(3000, () => console.log("Listening on port 3000..."));

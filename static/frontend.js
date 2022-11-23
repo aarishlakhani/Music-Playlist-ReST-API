6bfe087885dceb9b276fac2f4cb804d5df37b78a
aarishlakhani <alakha33@uwo.ca>
2022-11-23 05:49:48 -0500

    formating site

--- static/frontend.js
+++ static/frontend.js
@@ -3,6 +3,7 @@ var trackSearchForm = document.getElementById("tracksForm");
 var artistSearchForm = document.getElementById("artistForm");
 var albumSearchForm = document.getElementById("albumForm");
 var resultsDiv = document.getElementById("resultsDiv");
+var playlistAddForm = document.getElementById("playlistForm");
 
 trackSearchForm.addEventListener("submit", function (e) {
   e.preventDefault(); //prevents default ghost clicks
@@ -18,15 +19,19 @@ trackSearchForm.addEventListener("submit", function (e) {
   )
     .then((res) => res.json())
     .then(function (data) {
-      const paragraph = document.createElement("p");
-
       while (resultsDiv.hasChildNodes()) {
         resultsDiv.removeChild(resultsDiv.firstChild);
       }
 
-      paragraph.appendChild(document.createTextNode(JSON.stringify(data)));
-      resultsDiv.appendChild(paragraph);
-      console.log(data);
+      for (let i = 0; i < Object.keys(data).length; i++) {
+        const paragraph = document.createElement("p");
+        paragraph.appendChild(
+          document.createTextNode(JSON.stringify(data[i]) + "\n")
+        );
+        resultsDiv.appendChild(paragraph);
+      }
+
+      console.log(JSON.stringify(data, null, 2));
     })
     .catch((err) => console.log(err));
 });
@@ -51,8 +56,13 @@ artistSearchForm.addEventListener("submit", function (e) {
         resultsDiv.removeChild(resultsDiv.firstChild);
       }
 
-      paragraph.appendChild(document.createTextNode(JSON.stringify(data)));
-      resultsDiv.appendChild(paragraph);
+      for (let i = 0; i < Object.keys(data).length; i++) {
+        const paragraph = document.createElement("p");
+        paragraph.appendChild(
+          document.createTextNode(JSON.stringify(data[i]) + "\n")
+        );
+        resultsDiv.appendChild(paragraph);
+      }
 
       console.log(data);
     })
@@ -79,8 +89,13 @@ albumSearchForm.addEventListener("submit", function (e) {
         resultsDiv.removeChild(resultsDiv.firstChild);
       }
 
-      paragraph.appendChild(document.createTextNode(JSON.stringify(data)));
-      resultsDiv.appendChild(paragraph);
+      for (let i = 0; i < Object.keys(data).length; i++) {
+        const paragraph = document.createElement("p");
+        paragraph.appendChild(
+          document.createTextNode(JSON.stringify(data[i]) + "\n")
+        );
+        resultsDiv.appendChild(paragraph);
+      }
       console.log(data);
     })
     .catch((err) => console.log(err));
